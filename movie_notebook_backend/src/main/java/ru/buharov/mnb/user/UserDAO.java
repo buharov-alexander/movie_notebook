46e7ba81ade35aa4bd521eca801e1b7c53375992
buharov-alexander <buharovsasha@yandex.ru>
2019-11-12 22:55:14 +0700

    move UserDAO

--- movie_notebook_backend/src/main/java/ru/buharov/mnb/user/UserDAO.java
+++ movie_notebook_backend/src/main/java/ru/buharov/mnb/user/UserDAO.java
@@ -1,11 +1,11 @@
-package ru.buharov.mnb.user.dao;
+package ru.buharov.mnb.user;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.buharov.mnb.user.domain.UserEntity;
 
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
-public interface UserDAO extends CrudRepository<UserEntity, Long> {
+interface UserDAO extends CrudRepository<UserEntity, Long> {
 
     UserEntity findByUsername(String username);
 }
