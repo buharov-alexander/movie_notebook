7d90ae6efb86556ca7839e2cd37c94596a948b61
buharov-alexander <buharovsasha@yandex.ru>
2019-11-16 00:08:40 +0700

    add security config

---
+++ movie_notebook_backend/src/main/java/ru/buharov/mnb/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package ru.buharov.mnb.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.buharov.mnb.user.CustomUserDetailsService;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth,
+                                CustomUserDetailsService userDetailsService,
+                                PasswordEncoder encoder) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder);
+    }
+}
