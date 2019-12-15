3cb897a2b9d55f69dfb26fda09d5c8caca1cad35
buharov-alexander <buharovsasha@yandex.ru>
2019-12-15 11:09:23 +0700

    add frontend router

--- movie_notebook_web/src/App.js
+++ movie_notebook_web/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import configureStore from 'store/configureStore';
+import Router from 'routing/Router';
 import './App.css';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-    <div>Test</div>
+    <Router />
   </Provider>
 );
 
---
+++ movie_notebook_web/src/routing/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import NavigationBar from 'components/header/NavigationBar';
+import MoviesPage from 'pages/movies/MoviesPage';
+
+const Router = () => (
+  <BrowserRouter basename="/mnb">
+    <div id="router">
+      <main className="main">
+        <NavigationBar />
+        <Switch>
+          <Route path="/movies" component={MoviesPage} />
+        </Switch>
+      </main>
+    </div>
+  </BrowserRouter>
+);
+
+export default Router;
