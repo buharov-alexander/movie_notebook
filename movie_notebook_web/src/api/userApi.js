4941c51463e65d8266e8961ed0ef033c611838da
buharov-alexander <buharovsasha@yandex.ru>
2020-02-09 20:32:41 +0700

    fix url

--- movie_notebook_web/src/api/userApi.js
+++ movie_notebook_web/src/api/userApi.js
@@ -1,4 +1,4 @@
-export const signInRequest = ({ username, password }) => fetch('/mnb/ui/login', {
+export const signInRequest = ({ username, password }) => fetch('/mnb/webui/login', {
   method: 'post',
   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   body: `username=${username}&password=${password}`,
