e269527574ff542738e91ad1241036b8fdd459c1
buharov-alexander <buharovsasha@yandex.ru>
2020-02-18 23:43:10 +0700

    enable csrf on backend

--- movie_notebook_backend/src/main/java/ru/buharov/mnb/security/WebSecurityConfig.java
+++ movie_notebook_backend/src/main/java/ru/buharov/mnb/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import ru.buharov.mnb.user.CustomUserDetailsService;
 
 @EnableWebSecurity
@@ -14,7 +15,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                     .antMatchers("/*.json",
                         "/static/js/*",
