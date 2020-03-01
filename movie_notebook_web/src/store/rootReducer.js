956a2b1c55fb806843edc5f005acde0e1e0451cd
buharov-alexander <buharovsasha@yandex.ru>
2020-03-01 10:59:05 +0700

    minor fix

--- movie_notebook_web/src/store/rootReducer.js
+++ movie_notebook_web/src/store/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
 import moviesReducer from 'pages/movies/moviesReducer';
 import searchReducer from 'pages/search/searchReducer';
 import pagesReducer from 'pages/pagesReducer';
